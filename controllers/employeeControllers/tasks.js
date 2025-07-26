9f8c3cc7065fab11af3f1eccadb0f1a87106ac54
TusharS0012 <tushardestro17@gmail.com>
2025-07-26 23:15:18 +0530

    Refactor employee and task controllers for improved error handling and response structure

--- controllers/employeeControllers/tasks.js
+++ controllers/employeeControllers/tasks.js
@@ -2,9 +2,43 @@
 import Task from "../../models/taskModel.js"; // Import the Task model
 
 export const getMyTasks = async (req, res) => {
-    // Controller logic for getting tasks assigned to the authenticated employee
+  const employeeId = req.params.id;
+  try {
+    const employee = await Task.find({ assignedTo: employeeId });
+    if (!employee) {
+      return res.status(404).json({ message: "Employee not found" });
+    }
+    res
+      .status(200)
+      .json({ message: "My tasks fetched successfully", tasks: employee });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error fetching my tasks",
+      error: error.message,
+    });
+  }
 };
 
 export const updateTaskStatus = async (req, res) => {
-    // Controller logic for updating the status of a specific task
+  const taskId = req.params.id;
+  try {
+    const task = await Task.findById(taskId);
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    const { status } = req.body;
+    if (!status) {
+      return res
+        .status(400)
+        .json({ message: "All fields (status) are required" });
+    }
+    task.status = status;
+    await task.save();
+    res.status(200).json({ message: "Task status updated successfully", task });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error updating task status",
+      error: error.message,
+    });
+  }
 };
