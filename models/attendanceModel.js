05079ec3dbf8ecbb94376cb94670b58effcdaa78
TusharS0012 <tushardestro17@gmail.com>
2025-07-27 00:07:57 +0530

    Refactor leave management controllers and models for improved functionality and consistency

--- models/attendanceModel.js
+++ models/attendanceModel.js
@@ -1,10 +1,21 @@
 import mongoose from "mongoose";
 
-const attendanceSchema = new mongoose.Schema({
-  employeeId: { type: mongoose.Schema.Types.ObjectId, ref: "Employee", required: true },
-  date: { type: Date, required: true },
-  status: { type: String, enum: ["Present", "Absent", "Leave"], required: true }
-}, { timestamps: true });
+const attendanceSchema = new mongoose.Schema(
+  {
+    employeeId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Employee",
+      required: true,
+    },
+    date: { type: Date, required: true },
+    status: {
+      type: String,
+      enum: ["Present", "Absent", "Leave"],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
 const attendanceModel = mongoose.model("Attendance", attendanceSchema);
 
